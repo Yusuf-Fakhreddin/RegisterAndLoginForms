4c4b07159630cf85c29bca800beb6c5abd71db2c
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-02-05 10:35:22 +0200

    MVC Login form, success, failure

---
+++ RegisterAndLoginForms/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginForms.Models;
+
+namespace RegisterAndLoginForms.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // expecting a UserModel Obj 
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            // sending the UserModel obj to the success page
+            if (userModel.UserName == "Hello World" && userModel.Password == "123456")
+                return View("LoginSuccess", userModel);
+            else
+                return View("LoginFailure", userModel);
+
+        }
+    }
+}
